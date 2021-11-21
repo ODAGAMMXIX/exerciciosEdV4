53df2ed62065b09cacad410594d110b2bf6584ef
Odaga <fabio.odaguiri@fatec.sp.gov.br>
2021-11-21 17:04:24 -0300

    ffffff

--- src/br/com/fatec/sort/shell/ShellSort.java
+++ src/br/com/fatec/sort/shell/ShellSort.java
@@ -6,15 +6,16 @@ public class ShellSort {
     public static void sort(int[] arr) {
         int counter, traversingIndex, key, gap;
 
-        for (gap = arr.length /2; gap > 0 ; gap=gap/2) {
-            for (counter = gap; counter < arr.length; counter++) {
-                key = arr[counter];
-                traversingIndex = counter - gap;
+        for (gap = arr.length /2; gap > 0 ; gap=gap/2) { System.out.println("Gap= "+gap);
+            for (counter = gap; counter < arr.length; counter++) { System.out.println("Counter= "+counter);
+                key = arr[counter]; System.out.println("Key= "+key);
+                traversingIndex = counter - gap; System.out.println("tIndex= "+traversingIndex);
+                Utils.printArray(arr);
                 while (traversingIndex >= 0 && arr[traversingIndex] > key) {
-                    arr[traversingIndex + gap] = arr[traversingIndex];
-                    traversingIndex = traversingIndex - gap;
-                }
-                arr[traversingIndex + gap] = key;
+                    arr[traversingIndex + gap] = arr[traversingIndex]; System.out.println("Gap= "+gap);
+                    traversingIndex = traversingIndex - gap; System.out.println("tIndex= "+traversingIndex);
+                }                
+                arr[traversingIndex + gap] = key; System.out.println("Valor Transportado = "+arr[traversingIndex + gap]);
 
             }
         }
