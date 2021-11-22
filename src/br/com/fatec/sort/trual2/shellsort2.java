4a60e9751ef43956358661fff29f17a83dfe8649
Odaga <fabio.odaguiri@fatec.sp.gov.br>
2021-11-22 20:46:01 -0300

    gggggggggggggg

--- src/br/com/fatec/sort/trual2/shellsort2.java
+++ src/br/com/fatec/sort/trual2/shellsort2.java
@@ -1,41 +1,40 @@
-package br.com.fatec.sort.shell;
+
+package br.com.fatec.sort.trual2;
 
 import br.com.fatec.sort.utils.Utils;
 
-public class ShellSort {
+public class shellsort2 {
     public static void sort(int[] arr) {
         int counter, traversingIndex, vtransportado, gap;
 
-        for (gap = arr.length /2; gap > 0 ; gap=gap/2) { System.out.println("VÃO= "+gap);
+        for (gap = arr.length /2; gap > 0 ; gap=gap/2) 
             //DIVIDE O CONJUNTO EM 2 PARTES, REPETE ATÉ ZERO
             for (counter = gap; counter < arr.length; counter++) { //System.out.println("Counter= "+counter);
                 // CONTADOR = VÃO OU METADE DO (SUB)CONJUNTO; SENDO < VALOR DA ÚLTIMA POSIÇÃO DO CONJUNTO; INCREMENTE 
-                 System.out.println("Valor À DIREITA = "+arr[counter]);
-                vtransportado = arr[counter]; System.out.println("Valor Transportado DA DIREITA= "+vtransportado);
+                 
+                vtransportado = arr[counter]; 
                 //VALOR TRANSPORTADO = VALOR Á DIREITA DO VÃO (VALOR NA POSIÇÃO DO CONTADOR)
                 traversingIndex = counter - gap;                 //ÍNDICE=CONTADOR - VÃO
                 Utils.printArray(arr);
                 while (traversingIndex >= 0 && arr[traversingIndex] > vtransportado) {
                     //COMPARAÇÃO: VTRANSPORTADO X VALORES À SUA ESQUERDA;
                             //COMPARAÇÃO: ENQUANTO ÍNDICE >=ZERO && VALOR NA POSIÇÃO DAQUELE ÍNDICE > VALOR TRANPORTADO
-                    arr[traversingIndex + gap] = arr[traversingIndex]; System.out.println("!!!!posição da esquerda!!!!"+arr[traversingIndex]);
+                    arr[traversingIndex + gap] = arr[traversingIndex]; 
                     //JOGA O MAIOR PRA DIREITA
                             //POSIÇÃO DO ÍNDICE + VÃO = VALOR NA POSIÇÃO DO ÍNDICE
-                    traversingIndex = traversingIndex - gap; //System.out.println("recebe o da esquerda= "+arr[traversingIndex]);
+                    traversingIndex = traversingIndex - gap; 
                     //ÍNDICE-VÃO
                     Utils.printArray(arr);
                 }                
-                arr[traversingIndex + gap] = vtransportado; //System.out.println("Valor Transportado DIREITA OU ESQUERDA = "+arr[traversingIndex + gap]);
+                arr[traversingIndex + gap] = vtransportado; 
                 //TRAZ O VALOR TRANSPORTADO=MENOR PARA ESQUERDA
                             //VALOR NA POSIÇÃO ÍNDICE+VÃO = VALOR TRANSPORTADO
                 Utils.printArray(arr);
             }
         }
-    }
-
+    
     public static void main(String[] args) {
-        // int arr[] = {64,25,12,22,11};
-        int arr[] = {12,34,54,2,3};
+        int arr[] = {64,25,12,22,11};
         System.out.println("Unsorted array:");
         Utils.printArray(arr);
         sort(arr);
